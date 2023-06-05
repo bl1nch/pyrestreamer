5a9cc16e29e8525da84be94807df728c0b5400ca
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-05 18:04:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
         streams.add(Stream(**element))
 
     streams.start()
-    socketio.run(app, host='127.0.0.1', port=80, certfile='certificate.pem', keyfile='privatekey.pem')
+    socketio.run(app, host='127.0.0.1', port=5000, ssl_context=('certificate.pem', 'privatekey.pem'))
