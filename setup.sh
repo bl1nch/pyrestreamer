ddc96de1e57a0537a21d3865ed0c19c45354a029
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-11 02:31:00 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
 
-activate(){
+activate() {
     . .venv/bin/activate
     pip install -r requirements.txt
     deactivate
@@ -11,7 +11,7 @@ mkdir logs
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y vlc python3-virtualenv python3-pip tmux openssl
-openssl req -newkey rsa:2048  -x509  -sha512  -days 365 -nodes -out certificate.pem -keyout privatekey.pem
+openssl req -newkey rsa:4096  -x509  -sha512  -days 365 -nodes -out certificate.pem -keyout key.pem
 virtualenv .venv
 tmux new -d -s pyrestreamer
 activate
