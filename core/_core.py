f9ad9b509bef6d214c164329bdd277e6d0f0c93e
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-10 19:05:00 +0600

    Update _core.py

--- core/_core.py
+++ core/_core.py
@@ -1,10 +1,17 @@
 from config import create_app, setup_logging, start_monitoring
 from pysondb import getDb
 from extensions.streaming import StreamPool
+from flask_apscheduler import APScheduler
 
 
+ssl, host, port = False, '127.0.0.1', 5000
 app, socketio = create_app()
 log = setup_logging(socketio=socketio)
 monitor = start_monitoring(socketio=socketio)
 db = getDb(filename="db.json", log=app.debug)
-streams = StreamPool(quiet=app.debug)
+scheduler = APScheduler()
+scheduler.init_app(app)
+streams = StreamPool(quiet=not app.debug,
+                     address=('https://' if ssl else 'http://') + host + ':' + str(port),
+                     scheduler=scheduler)
+scheduler.start()
