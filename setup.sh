d5df0ba01bede2e967ed55a0203af6a57a0aec57
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-10 21:14:33 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ mkdir logs
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y vlc python3-virtualenv python3-pip tmux openssl
-openssl req -newkey rsa:4096  -x509  -sha512  -days 365 -nodes -out certificate.pem -keyout privatekey.pem
+openssl req -newkey rsa:2048  -x509  -sha512  -days 365 -nodes -out certificate.pem -keyout privatekey.pem
 virtualenv .venv
 tmux new -d -s pyrestreamer
 activate
