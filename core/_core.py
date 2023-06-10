b87350b24b8c8f13dd8f8653e9338942dafd1fbf
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-11 02:38:07 +0600

    Update _core.py

--- core/_core.py
+++ core/_core.py
@@ -4,14 +4,12 @@ from extensions.streaming import StreamPool
 from flask_apscheduler import APScheduler
 
 
-ssl, host, port = False, '127.0.0.1', 5000
+host, port = '127.0.0.1', 5000
 app, socketio = create_app()
 log = setup_logging(socketio=socketio)
 monitor = start_monitoring(socketio=socketio)
 db = getDb(filename="db.json", log=app.debug)
 scheduler = APScheduler()
 scheduler.init_app(app)
-streams = StreamPool(quiet=not app.debug,
-                     address=('https://' if ssl else 'http://') + host + ':' + str(port),
-                     scheduler=scheduler)
+streams = StreamPool(quiet=not app.debug, scheduler=scheduler)
 scheduler.start()
