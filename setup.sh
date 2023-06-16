5c0681c1dfc0ec99ae35a23599c5f8cb27cb40a8
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-16 23:54:12 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 activate() {
     . .venv/bin/activate
     pip install -r requirements.txt
+    pysondb create db.json
     deactivate
 }
 
 mkdir logs
-pysondb create db.json
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y vlc python3-virtualenv python3-pip tmux openssl
