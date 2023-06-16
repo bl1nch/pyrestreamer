3139324bbc2d5671e206fb16f2f2bb5931ad8b97
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-16 23:58:58 +0600

    Update _core.py

--- core/_core.py
+++ core/_core.py
@@ -1,7 +1,7 @@
 from config import create_app, setup_logging, start_monitoring
 from pysondb import getDb
 from extensions.streaming import StreamPool
-from flask_apscheduler import APScheduler
+from apscheduler.schedulers.gevent import GeventScheduler
 
 
 host, port = '0.0.0.0', 5050
@@ -9,7 +9,6 @@ app, socketio = create_app()
 log = setup_logging(socketio=socketio)
 monitor = start_monitoring(socketio=socketio)
 db = getDb(filename="db.json", log=app.debug)
-scheduler = APScheduler()
-scheduler.init_app(app)
+scheduler = GeventScheduler()
 streams = StreamPool(quiet=not app.debug, scheduler=scheduler)
 scheduler.start()
