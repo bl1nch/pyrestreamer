67fb949d79b11e4669822db787fbbdfb9a3cfad6
bl1nch <130155870+bl1nch@users.noreply.github.com>
2023-06-16 23:53:25 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ activate() {
 }
 
 mkdir logs
+pysondb create db.json
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y vlc python3-virtualenv python3-pip tmux openssl
